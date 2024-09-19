07b6d202fb236ceb463005687851f14389dd7368
Yiannis Karamitros <yiannis.karamitros0817@gmail.com>
2024-09-19 14:40:41 -0400

    new updates SPD Work 2

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -112,15 +112,11 @@ const work = [
   },
   {
     name: 'Sigma Phi Delta Professional Engineering Fraternity',
-    position: 'Various Positions',
+    position: 'Various Positions (Fundraising Chair, Guide, Chaplain, Creative Merchandise Chair, ) ',
     url: 'https://lehighspd.org/',
     startDate: '2021-04-10',
     summary: 'Bethlehem, PA',
     highlights: [
-      'Fundraising Chair',
-      'Guide',
-      'Chaplain',
-      'Creative Merchandise Chair',
       'Oversee wellbeing of 60+ active members, assisting with internal problem solving and conflict resolution by being the mediator',
       'Interpersonal liaison between members and executive leaders, meeting weekly with the executive board for fraternity wellbeing',
       'Conduct one-on-one meetings with members dealing with personal or academic issues to support and share resources they may need',
