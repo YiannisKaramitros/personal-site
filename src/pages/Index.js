1cc60b95528a96a363ef7ee49dfaaab9b2bef134
Yiannis Karamitros <yiannis.karamitros0817@gmail.com>
2025-02-13 10:01:51 -0500

    test in febuary

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,8 +12,7 @@ const Index = () => (
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            A beautiful
           </p>
         </div>
       </header>
