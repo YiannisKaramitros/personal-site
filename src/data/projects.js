42617f9f97a1e480df29f1fb48b1d3c7c9dc7dd0
Yiannis Karamitros <yiannis.karamitros0817@gmail.com>
2025-02-19 10:37:33 -0500

    some good updatesrr

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,17 +1,5 @@
 const data = [
   {
-    title: 'Merck PPX: Machine Assisted Contextualization',
-    subtitle: 'Capstone Class Project',
-    image: '/images/projects/MerckPPX.jpg',
-    date: 'January 2023 - December 2023',
-    desc:
-      'Built for our Capstone project at school. '
-      + 'Merck PPX is a web application that allows Merck '
-      + 'employees to run labels through a machine learning model'
-      + 'instead of doing it one by one.',
-  },
-
-  {
     title: 'Global CBDC Consensus: Hierarchial Stellar Consensus Protocol',
     subtitle: 'CBDC Research Project',
     image: '/images/projects/CBDCPoster.jpg',
@@ -21,6 +9,21 @@ const data = [
       + 'aims to better the world of currency exchange  '
       + 'through a hierarchial model. Stellar Consensus Protocol '
       + 'and Soroban implemented in the design.',
+  },
+  {
+    title: 'Merck PPX: Machine Assisted Contextualization',
+    subtitle: 'Capstone Class Project',
+    image: '/images/projects/MerckPPX.jpg',
+    date: 'January 2023 - December 2023',
+    desc:
+      'Built for a capstone project during my undergraduate degree. '
+      + 'Merck PPX is a web application that allows Merck '
+      + 'employees to run labels through a machine learning model '
+      + 'instead of doing it manually one by one. '
+      + 'We Utilized Python and ML/NLP libraries to test, train, and validate machine '
+      + 'learning approaches to a 10,000+ data point manufacturing lake. And '
+      + 'Developed an easy web application leveraging ML/NLP techniques to '
+      + 'reduce human effort and improve accuracy for contextualizing data',
   },
 ];
 
